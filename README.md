73360b3b01b434ba5abbbffb5dfe0c4c62d031bd
firedot1 <zcheng1@montgomerycollege.edu>
2020-04-21 14:16:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"Program for printing two variables names." 
---
+++ src/MathTwo.java
@@ -0,0 +1,8 @@
+public class MathTwo {
+    public static void main (String args[]) {
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+        System.out.println("The mynumber value is: " + mynumber);
+        System.out.println("The myothernumber value is: " + myothernumber);
+    }
+}
